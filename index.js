888549ffcb4e17e094e321b2c7f8f7c801b30519
shaiMatz <shyshir@gmail.com>
2023-05-13 02:02:04 +0300

    added port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server.js')
-const port = 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`);
     console.log(`Website: \x1b[33m%s\x1b[0m`, `http://localhost:${port}`);
