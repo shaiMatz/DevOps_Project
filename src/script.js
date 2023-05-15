65bd551c350e4c3d5c5f62b1fb497a3bf28190c0
Yossi Elgazari <yossielg98@gmail.com>
2023-05-15 13:30:36 +0300

    done

--- src/script.js
+++ src/script.js
@@ -1,8 +1,3 @@
-if (typeof window !== 'undefined') {
-const form = document.getElementById("myForm");
-form.addEventListener("submit", saveGradesToLocalStorage);
-}
-
 // function to validate the student name
 function validateName(name) {
   const regex = /^[a-zA-Z\s-]+$/;
@@ -10,9 +5,8 @@ function validateName(name) {
 }
 
 // function to handle the form submission
-function saveGradesToLocalStorage(event) {
-  event.preventDefault();
-
+function saveGradesToLocalStorage() {
+  console.log("saveGradesToLocalStorage() called");
   const nameInput = document.getElementById("name");
   const grade1Input = document.getElementById("grade1");
   const grade2Input = document.getElementById("grade2");
@@ -22,7 +16,7 @@ function saveGradesToLocalStorage(event) {
   const name = nameInput.value;
   if (!validateName(name)) {
     alert("Please enter a valid name (containing no digits or special characters).");
-    return;
+    return 0;
   }
 
   const grade1 = parseInt(grade1Input.value);
@@ -32,7 +26,7 @@ function saveGradesToLocalStorage(event) {
 
   if (isNaN(average) || average < 0 || average > 100) {
     alert("Please enter valid grades (between 0 and 100).");
-    return;
+    return 0;
   }
 
   const csvString = `${name},${grade1},${grade2},${grade3}\n`;
@@ -44,7 +38,7 @@ function saveGradesToLocalStorage(event) {
     localStorage.setItem("grades", existingData + csvString);
   }
 
-  form.reset();
+  return 1;
 }
 
 // function to display the grades
@@ -52,18 +46,15 @@ function displayGrades() {
   const grades = localStorage.getItem("grades");
   const gradesArray = grades.split("\n");
   const tableBody = document.getElementById("tableBody");
-  document.getElementById('table').style.visibility = 'visible'; 
+  document.getElementById('table').style.visibility = 'visible';
   document.getElementById('table').style.display = 'table';
   document.getElementById('table').style.borderCollapse = 'collapse';
 
   // Check if there is any existing data in the table
   const existingData = tableBody.innerHTML.trim();
-
   // If there is no existing data or it is different from the new data, update the table
   if (existingData !== grades) {
-    // Clear the existing table
     tableBody.innerHTML = "";
-    // Create table header row
     const headerRow = document.createElement("tr");
     const nameHeader = document.createElement("th");
     const nameHeaderText = document.createTextNode("Name");
@@ -107,11 +98,21 @@ function displayGrades() {
   }
 }
 
-try {
-module.exports = { validateName, saveGradesToLocalStorage };
-} catch (e) {console.log("module.exports not found.. not big deal")}
+// function to clear local storage
+function clearLocalStorage() {
+  const confirmation = confirm("Are you sure you want to delete all saved data?");
 
-if (typeof window !== 'undefined')
-{
+  if (confirmation) {
+    localStorage.clear();
+    alert("All saved data has been deleted.");
+  }
+}
+
+
+try {
+  module.exports = { validateName, saveGradesToLocalStorage };
+} catch (e) { console.log("module.exports not found.. not big deal") }
+
+if (typeof window !== 'undefined') {
   window.validateName = validateName;
 }
